375c838d37ec854d2c67cee45464fefd396b1399
Kris <kris.weber.1994@gmail.com>
2017-09-19 11:38:55 -0700

    Add solution for splitting an array into negative and positive sections in order of absolute value for each side

---
+++ splitArrPosNeg.js
@@ -0,0 +1,26 @@
+let sortArr = (arr) => {
+
+  var negs = [];
+  var pos = [];
+
+  for(let i = 0; i < arr.length; i++) {
+
+    if(arr[i] > 0) {
+      pos.push(arr[i]);
+    } else if(arr[i] < 0) {
+      negs.push(arr[i]);
+    } else if(arr[i] === 0) {
+      pos.unshift(arr[i]);
+    }
+
+  }
+
+  for(let j = 0; j < pos.length; j++) {
+    negs.push(pos[j]);
+  }
+
+  return negs;
+
+};
+
+sortArr([1, -2, -3, -4, 5, -6, 7, -8, 9, -10, -11, -12, 0, 20]);
