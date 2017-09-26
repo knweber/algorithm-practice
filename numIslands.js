134069a660c7407eeb929a88bc0c0d49624e6f5a
Kris <kris.weber.1994@gmail.com>
2017-09-25 19:50:46 -0700

    Add leetcode number of islands problem

---
+++ numIslands.js
@@ -0,0 +1,32 @@
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+let numIslands = (grid) => {
+    let count = 0;
+    for(let i = 0; i < grid.length; i++) {
+        for(let j = 0; j < grid[0].length; j++) {
+            if(grid[i][j] === '1') {
+                dfs(grid,i,j);
+                count++;
+            }
+        }
+    }
+    return count;
+};
+
+let dfs = (grid, i , j) => {
+    if(i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] === '1') {
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+};
+
+// Example input:
+// 11110
+// 11010
+// 11000
+// 00000
+//
+// Output: 1
