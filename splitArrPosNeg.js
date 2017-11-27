3fbe402dd70ad7b5798f2ac0b9dbcb4645f17a91
Kris <kris.weber.1994@gmail.com>
2017-11-27 08:44:30 -0800

    Add solution for rearranging positive and negative nums

--- splitArrPosNeg.js
+++ splitArrPosNeg.js
@@ -1,28 +1,55 @@
-let sortArr = (arr) => {
+function rearrange(arr) {
+  if(!arr) {
+    return null;
+  }
 
-  var negs = [];
-  var pos = [];
+  if(arr.length === 1) {
+    return arr;
+  }
 
-  for(let i = 0; i < arr.length; i++) {
-
-    if(arr[i] > 0) {
-      pos.push(arr[i]);
-    } else if(arr[i] < 0) {
-      negs.push(arr[i]);
-    } else if(arr[i] === 0) {
-      pos.unshift(arr[i]);
+  for(var i = 1; i < arr.length; i++) {
+    var key = arr[i];
+    if(key > 0) {
+      continue;
     }
 
+    var j = i-1;
+    while(j >= 0 && arr[j] > 0) {
+      arr[j+1] = arr[j];
+      j = j-1;
+    }
+    arr[j+1] = key;
   }
+  return arr;
+}
 
-  for(let j = 0; j < pos.length; j++) {
-    negs.push(pos[j]);
-  }
 
-  return negs;
 
-};
-
-sortArr([1, -2, -3, -4, 5, -6, 7, -8, 9, -10, -11, -12, 0, 20]);
+// let sortArr = (arr) => {
+//
+//   var negs = [];
+//   var pos = [];
+//
+//   for(let i = 0; i < arr.length; i++) {
+//
+//     if(arr[i] > 0) {
+//       pos.push(arr[i]);
+//     } else if(arr[i] < 0) {
+//       negs.push(arr[i]);
+//     } else if(arr[i] === 0) {
+//       pos.unshift(arr[i]);
+//     }
+//
+//   }
+//
+//   for(let j = 0; j < pos.length; j++) {
+//     negs.push(pos[j]);
+//   }
+//
+//   return negs;
+//
+// };
+//
+// sortArr([1, -2, -3, -4, 5, -6, 7, -8, 9, -10, -11, -12, 0, 20]);
 
 // ==> [ -2, -3, -4, -6, -8, -10, -11, -12, 0, 1, 5, 7, 9, 20 ]
